daed8d881e279a0bb654bce2ff71df2048cf2a6c
Parth Kadecha <parthkadecha25@gmail.com>
2020-10-19 02:21:28 +0530

    Cosmetic changes

--- MVCSalesforceCRUD/MVCSalesforceCRUD/Scripts/Main.js
+++ MVCSalesforceCRUD/MVCSalesforceCRUD/Scripts/Main.js
@@ -1,19 +1,18 @@
 ﻿(function (m, $, undefined) {
 
-    var globalDeleteLeadUrl = "";
-
     var globalDeleteOpportunityUrl = "";
 
-    m.Init = function (deleteLeadUrl, deleteOpportunityUrl) {
-        globalDeleteLeadUrl = deleteLeadUrl;
+    m.Init = function (deleteOpportunityUrl) {
         globalDeleteOpportunityUrl = deleteOpportunityUrl;
     }
 
+    // After "Delete Opportunity" operation
     $("#confirm-delete").on("hide.bs.modal", function (e) {
         $("#btnLoader").hide();
         $(this).find(".jsBtnDelete").removeAttr("data-confirmedid");
     });
 
+    // Asking for confirmation to delete the opportunity
     $(document).on("click", ".jsConfirmDeleteOpportunity", function (e) {
         var opportunityId = $(this).attr("id");
         $("#btnDeleteOpportunity").attr("data-confirmedid", opportunityId);
@@ -23,6 +22,8 @@
         $("#confirm-delete").modal("show");
     });
 
+    // On Deleting the opportunity
+    // Calling the controller method for deleting the opportunity
     $(document).on("click", "#btnDeleteOpportunity", function (e) {
         var $this = $(this);
         $this.hide();
@@ -64,56 +65,6 @@
                 $("#status-delete").find("#delete-fail").show();
                 $("#status-delete").modal("show");
                 $this.removeAttr("data-confirmedid");
-                return false;
-            }
-        });
-    });
-
-    $(document).on("click", ".jsConfirmDeleteLead", function (e) {
-        var leadId = $(this).attr("id");
-        $("#btnDeleteLead").attr("data-confirmedid", leadId);
-        $("#btnDeleteLead").show();
-        $("#btnCancel").show();
-        $("#btnLoader").hide();
-        $("#confirm-delete").modal("show");
-    });
-
-    $(document).on("click", "#btnDeleteLead", function (e) {
-        var $this = $(this);
-        $this.hide();
-        $("#btnCancel").hide();
-        $("#btnLoader").show();
-
-        var confirmedId = $this.attr("data-confirmedid");
-        var args = JSON.stringify({ "leadid": confirmedId });
-
-        $.ajax({
-            cache: false,
-            contentType: "application/json;charset=utf-8",
-            type: "POST",
-            dataType: "JSON",
-            url: globalDeleteLeadUrl,
-            data: args,
-            success: function (data) {
-                $("#confirm-delete").modal("hide");
-                if (data.IsDeleted) {
-                    $("#lead_" + confirmedId).remove();
-                    $("#status-delete").find(".jsSuccessMsg").text(data.Details);
-                    $("#status-delete").find(".jsErrorMsg").text('');
-                    $("#status-delete").find("#delete-success").show();
-                    $("#status-delete").find("#delete-fail").hide();
-                } else {
-                    $("#status-delete").find(".jsSuccessMsg").text('');
-                    $("#status-delete").find(".jsErrorMsg").text(data.Details);
-                    $("#status-delete").find("#delete-success").hide();
-                    $("#status-delete").find("#delete-fail").show();
-                }
-                $("#status-delete").modal("show");
-                $this.removeAttr("data-confirmedid");
-                return false;
-            },
-            error: function (err) {
-                alert("failed");
                 return false;
             }
         });
