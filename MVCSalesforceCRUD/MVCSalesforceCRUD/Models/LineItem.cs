430a21d93034270d492ff69bbfc81fd48ed6326f
Parth Kadecha <parthkadecha25@gmail.com>
2020-10-18 16:38:29 +0530

    Added basic models and WSDL for SalesForce

---
+++ MVCSalesforceCRUD/MVCSalesforceCRUD/Models/LineItem.cs
@@ -0,0 +1,13 @@
+﻿namespace MVCSalesforceCRUD.Models
+{
+    // Model for holding Opportuniy Line item details
+    public class LineItem
+    {
+        public string SalesPrice; // UnitPrice. This is the Sales Price of the Product (Required).
+        public string ListPrice; // This is the List Price of the Product(Required).
+        public int Quantity; // This is the quantity of this line item for the opportunity (Required).
+        public string Description; // This is the Description of the Line Item, as text (Required).
+        public string OpportunityID; // This is the ID of the opportunity, as text (Required).
+        public string PriceBookEntryID; // This is the id of the price book entry, as text (Required).
+    }
+}
---
+++ MVCSalesforceCRUD/MVCSalesforceCRUD/Models/Opportunity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MVCSalesforceCRUD.Models
+{
+    // Model for holding Opportunity related details
+    public class Opportunity
+    {
+        public string Name; //Name of the Opportunity (Required).
+        public DateTime CloseDate; //The closing date of the Opportunity, This needs to be in the format of YYYY-MM-DD (Required).
+        public string StageName; // The stage at which the Opportunity is currently at. 
+        public string Probability; //The probability of the Opportunity being successful. (Required).
+        public string Amount; //The amount the Opportunity is potentially worth. (Required).
+        public string AccountID; //ID of the Account to create this Opportunity against. (Required).
+        public string PriceBookId; //This is the id of the price book, as text(Required)
+        public string OpportnityID; // ID of the opportunity
+    }
+}
